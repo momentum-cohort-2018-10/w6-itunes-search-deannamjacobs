eeda4fd57ecb1f4d99fe51c203523d3b7a4158af
deannamjacobs <jacobs.deanna.m@gmail.com>
2018-12-06 17:28:52 -0500

    add input field and submit button

---
+++ itunes.js
@@ -0,0 +1,10 @@
+document.getElementById("Submit").addEventListener("click", function () {
+    document.getElementById("Submit").innerText;
+})
+//This is the function for the Submit button.
+
+var input = document.createElement('myInput');
+// This identifies the input field.
+
+
+
