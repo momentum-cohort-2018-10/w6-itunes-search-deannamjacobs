6597adc8322d1e66e41dbc6c7a2eba6ae928f0f3
deannamjacobs <jacobs.deanna.m@gmail.com>
2018-12-08 21:03:23 -0500

    add search results

--- itunes.js
+++ itunes.js
@@ -1,8 +1,65 @@
-document.getElementById("Submit").addEventListener("click", function () {
-    document.getElementById("Submit").innerText;
-})
-//This is the function for the Submit button.
+const submitButton = document.getElementById("submit-button")
+const inputField = document.getElementById("input-field")
 
-var input = document.createElement('myInput');
-// This identifies the input field.
+submitButton.addEventListener("click", function (event) {
+    $.ajax({
+        url: "https://itunes-api-proxy.glitch.me/search?term=",
+        dataType: "json",
+        data: {
+            term: inputField.value,
+            // entity: "song",
+            // limit: 25,
+            // explicit: "No"
+        },
+
+        success: function (results) {
+
+            console.log(results);
+
+            let resultsDiv = document.getElementById("search-results")
+
+            let countP = document.createElement("p")
+
+            countP.innerText = "Total count: ${results.resultCount}"
+
+            resultsDiv.appendChild(countP)
+
+            for (let song of search.results) {
+
+                let songP = document.createElement("p")
+
+                let songLink = document.createElement("a")
+
+                songLink.href = song.songViewUrl
+
+                songLink.innerText = song.songName
+
+                songLink.innerText = song.artistName
+
+                songP.appendChild(songLink)
+
+                resultsDiv.appendChild(songP)
+            }
+        }
+    });
+})
+// $results.empty();
+
+// $.each(data.results, function (i, result) {
+//     if (i > 24) { return false; }
+
+
+
+
+// })
+// //This is the function for the Submit button.
+
+
+
+// var $input = $('<input type="search" />').appendTo(document.body),
+//     $results = $('<div class="results" />').appendTo(document.body);
+
+            // // $input.val('LeCrae');
+
+            // // function fetch(term) {
 
