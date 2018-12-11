5a14707bc267d919d594f622d690396733cd9294
deannamjacobs <jacobs.deanna.m@gmail.com>
2018-12-10 21:14:08 -0500

    remove audio, when click on title/artist in results, song plays

--- itunes.js
+++ itunes.js
@@ -10,7 +10,7 @@ submitButton.addEventListener("click", function (event) {
             media: "music",
             // attribute: genreIndex = 8,
             entity: "song",
-            // // limit: 25,
+            limit: 25,
             // explicit: "No",
         },
 
@@ -40,7 +40,7 @@ submitButton.addEventListener("click", function (event) {
 
                 song.href = song.trackViewUrl
 
-                songLink.href = song.trackViewUrl
+                songLink.href = song.previewUrl
 
                 songLink.innerText = song.trackName
 
@@ -58,6 +58,6 @@ submitButton.addEventListener("click", function (event) {
     });
 })
 
-const audioPlayer = document.getElementById("audio-player").addEventListener("click, function (event)") { }
+
 
 
